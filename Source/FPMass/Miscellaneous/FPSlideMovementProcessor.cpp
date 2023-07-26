8b355976e1de5ddf67a6bb820fddc381660032f9
fpwong <func.pwong@gmail.com>
2023-07-26 15:08:08 +1000

    Add slide movement processor

---
+++ Source/FPMass/Miscellaneous/FPSlideMovementProcessor.cpp
@@ -0,0 +1,229 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSlideMovementProcessor.h"
+
+#include "MassCommonFragments.h"
+#include "MassCommonTypes.h"
+#include "MassExecutionContext.h"
+#include "MassMovementFragments.h"
+
+#define FP_DRAW_DEBUG 0
+
+UFPSlideMovementProcessor::UFPSlideMovementProcessor()
+{
+	ExecutionFlags = static_cast<int32>(EProcessorExecutionFlags::All);
+	ExecutionOrder.ExecuteInGroup = UE::Mass::ProcessorGroupNames::Movement;
+	// bRequiresGameThreadExecution = false;
+}
+
+void UFPSlideMovementProcessor::ConfigureQueries()
+{
+	// MovementEntityQuery.AddTagRequirement<FMSBasicMovement>(EMassFragmentPresence::All);
+	MovementEntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadWrite);
+	//must have an FMassForceFragment and we are only reading it
+	MovementEntityQuery.AddRequirement<FMassForceFragment>(EMassFragmentAccess::ReadOnly);
+	MovementEntityQuery.AddRequirement<FAgentRadiusFragment>(EMassFragmentAccess::ReadOnly);
+	MovementEntityQuery.AddRequirement<FMassVelocityFragment>(EMassFragmentAccess::ReadWrite);
+	MovementEntityQuery.RegisterWithProcessor(*this);
+}
+
+void UFPSlideMovementProcessor::Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context)
+{
+	MovementEntityQuery.ForEachEntityChunk(EntityManager, Context, [](FMassExecutionContext& Context)
+	{
+		QUICK_SCOPE_CYCLE_COUNTER(STAT_MASS_MovementEntityQuery);
+
+		const int32 NumEntities = Context.GetNumEntities();
+
+		const TArrayView<FTransformFragment> TransformList = Context.GetMutableFragmentView<FTransformFragment>();
+
+		//This one is readonly, so we don't need Mutable
+		const TConstArrayView<FMassForceFragment> ForceList = Context.GetFragmentView<FMassForceFragment>();
+		const TConstArrayView<FAgentRadiusFragment> RadiusList = Context.GetFragmentView<FAgentRadiusFragment>();
+		const TArrayView<FMassVelocityFragment> VelocityList = Context.GetMutableFragmentView<FMassVelocityFragment>();
+
+		float DeltaTime = Context.GetDeltaTimeSeconds();
+
+		auto World = Context.GetWorld();
+
+		float GravityZ = World->GetGravityZ();
+
+		//Loop over every entity in the current chunk and do stuff!
+		for (int32 EntityIndex = 0; EntityIndex < NumEntities; ++EntityIndex)
+		{
+			FTransform& Transform = TransformList[EntityIndex].GetMutableTransform();
+
+			const FVector& Force = ForceList[EntityIndex].Value;
+			//+ FVector(0, 0, GravityZ);
+
+			FVector& Velocity = VelocityList[EntityIndex].Value;
+			Velocity += Force * DeltaTime;
+
+			const float& Radius = RadiusList[EntityIndex].Radius;
+
+			FVector Delta = Velocity * DeltaTime;
+
+			// perform the slide
+			FSlide Slide;
+			Slide.Shape = FCollisionShape::MakeSphere(Radius);
+			Slide.Position = Transform.GetLocation();
+			Slide.Remainder = Delta;
+			Slide.QueryParams = FCollisionQueryParams(FName("SlideMovementProcessor"), false, nullptr);
+
+			// cap out at three impacts
+			FHitResult Hit;
+			for (int It = 0; It < 3 && Slide.TryStep(World, Hit); ++It)
+			{
+				if (Hit.bStartPenetrating)
+				{
+					// Stuck! Draw a red warning sphere
+#if FP_DRAW_DEBUG
+					// DrawDebugSphere(World, Slide.Position, Slide.Shape.GetSphereRadius() + 5.0f, 16, FColor::Red);
+#endif
+					break;
+				}
+				else
+				{
+					// Impact! Draw the surface normal
+#if FP_DRAW_DEBUG
+					DrawDebugDirectionalArrow(World, Hit.ImpactPoint, Hit.ImpactPoint + 65.0f * Hit.Normal, 10.0f, FColor::Green, false, 0.5f);
+#endif
+				}
+			}
+
+			FVector OldLocation = Transform.GetLocation();
+			Transform.SetLocation(Slide.Position);
+
+			// Recalc velocity (is this correct?)
+			Velocity = (Slide.Position - OldLocation) / DeltaTime;
+		}
+	});
+}
+
+// ==========
+// FSlide
+// ==========
+
+bool FSlide::TryStep(const UWorld* World, FHitResult& Hit)
+{
+	// early out?
+	if (Remainder.IsNearlyZero(KINDA_SMALL_NUMBER))
+	{
+		return false;
+	}
+
+	// sweep forward
+	const bool bHit = DepenAndSweep(World, Hit, Position, Remainder, Rotation, Channel, Shape, QueryParams);
+	if (!bHit)
+	{
+		Position = Hit.TraceEnd;
+		Remainder = FVector(0.f);
+		return false;
+	}
+
+	// stuck?
+	if (Hit.bStartPenetrating)
+	{
+		return true;
+	}
+
+	// update position
+	Position = Hit.TraceStart + PullBackMove(Hit.Location - Hit.TraceStart);
+
+	// remainder is either projected onto the impact plane, or the seam between two acute impact-planes
+	// dot-product to check the angle
+	// cross product to calcuate the seam
+	const FVector Seam = (PrevNormal | Hit.Normal) < KINDA_SMALL_NUMBER ? (PrevNormal ^ Hit.Normal) : FVector(0.0f);
+	Remainder *= (1.f - Hit.Time);
+	Remainder = Seam.IsNearlyZero(0.1f) ?
+		FVector::VectorPlaneProject(Remainder, Hit.Normal) :
+		Remainder.ProjectOnTo(Seam);
+	PrevNormal = Hit.Normal;
+
+	return true;
+}
+
+FVector FSlide::PullBackMove(const FVector& Move)
+{
+	// magic number I found elsewhere in the Unreal Engine source code
+	const float Epsilon = 0.125f;
+	const float Dist = Move.Size();
+	return Dist > Epsilon ? Move * ((Dist - Epsilon) / Dist) : FVector(0.0f);
+}
+
+FCollisionShape FSlide::InflateShape(const FCollisionShape& InShape, float Amount)
+{
+	// helper function to add a "skin" to a shape
+	switch (InShape.ShapeType)
+	{
+		case ECollisionShape::Capsule:
+			return FCollisionShape::MakeCapsule(InShape.GetCapsuleRadius() + Amount, InShape.GetCapsuleHalfHeight() + Amount);
+		case ECollisionShape::Sphere:
+			return FCollisionShape::MakeSphere(InShape.GetSphereRadius() + Amount);
+		case ECollisionShape::Box:
+			return FCollisionShape::MakeBox(InShape.GetBox() + Amount);
+		default:
+			return InShape;
+	}
+}
+
+bool FSlide::DepenAndSweep(const UWorld* World, FHitResult& Hit, const FVector& Start, const FVector& Delta, const FQuat& Rot, ECollisionChannel InChannel, const FCollisionShape& InShape, const FCollisionQueryParams& Params)
+{
+	Hits.Reset();
+
+	// sweep with inflated skin
+	FCollisionShape Inflated = InflateShape(InShape, 0.25f);
+	const bool bSkinHit = World->SweepMultiByChannel(Hits, Start, Start + Delta, Rot, InChannel, Inflated, Params);
+
+	// no hits, even with skin?
+	if (!bSkinHit)
+	{
+		Hit.Init();
+		Hit.TraceStart = Start;
+		Hit.TraceEnd = Start + Delta;
+		Hit.Location = Start + Delta;
+		Hit.Time = 1.0f;
+		return false;
+	}
+
+	// have initial overlaps?
+	int NumInitialOverlaps = 0;
+	for (const FHitResult& It : Hits)
+	{
+		if (It.bStartPenetrating)
+		{
+			++NumInitialOverlaps;
+		}
+	}
+	if (NumInitialOverlaps == 0)
+	{
+		Hit = Hits.Last();
+		return bSkinHit;
+	}
+
+	// iteratively resolve penetration
+	FVector Fixup(0.0f);
+	for (int Iter = 0; Iter < 16; ++Iter)
+	{
+		float ErrorSum = 0.0f;
+		for (const FHitResult& It : Hits)
+		{
+			if (It.bStartPenetrating)
+			{
+				// take the dot-product of the Fixup and the Normal to determine how much
+				// of the penetration has already been taken care of.
+				const float Error = FMath::Max(0.0f, (It.PenetrationDepth + 0.125f) - (Fixup | It.Normal));
+				ErrorSum += Error;
+				Fixup += Error * It.Normal;
+			}
+		}
+		// If we've found a solution, stop iterating.
+		if (ErrorSum < KINDA_SMALL_NUMBER)
+		{
+			break;
+		}
+	}
+
+	// resweep from new start
+	return World->SweepSingleByChannel(Hit, Start + Fixup, Start + Fixup + Delta, Rot, InChannel, InShape, Params);
+}
---
+++ Source/FPMass/Miscellaneous/FPSlideMovementProcessor.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassProcessor.h"
+#include "UObject/Object.h"
+#include "FPSlideMovementProcessor.generated.h"
+
+UCLASS()
+class FPMASS_API UFPSlideMovementProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+
+public:
+	UFPSlideMovementProcessor();
+
+	FMassEntityQuery MovementEntityQuery;
+
+	virtual void ConfigureQueries() override;
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+};
+
+
+/**
+ * Helper class for slide collision against the world
+ * Credits: https://blog.littlepolygon.com/posts/sliding/
+ */
+class FSlide
+{
+public:
+	// out start position
+	FVector Position = FVector(0.0f);
+
+	// the desired offset vector
+	FVector Remainder = FVector(0.0f);
+
+	// the shape to slide
+	FCollisionShape Shape = FCollisionShape::MakeSphere(20.0f);
+
+	// the rotation of the shape
+	FQuat Rotation = FQuat::Identity;
+
+	// parameters used by unreal's collision-query system
+	FCollisionQueryParams QueryParams = FCollisionQueryParams::DefaultQueryParam;
+	ECollisionChannel Channel = ECC_Pawn;
+
+	// Advance Position/Remainder to the next impact
+	bool TryStep(const UWorld* World, FHitResult& Hit);
+
+	FVector PullBackMove(const FVector& Move);
+	FCollisionShape InflateShape(const FCollisionShape& InShape, float Amount);
+	bool DepenAndSweep(const UWorld* World, FHitResult& Hit, const FVector& Start, const FVector& Delta, const FQuat& Rot, ECollisionChannel InChannel, const FCollisionShape& InShape, const FCollisionQueryParams& Params);
+
+private:
+	FVector PrevNormal = FVector(0.0f);
+	TArray<FHitResult> Hits;
+};
