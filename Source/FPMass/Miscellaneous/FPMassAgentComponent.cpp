ee1723e99927b9c677a7986fc7b7ffe414cf809c
fpwong <func.pwong@gmail.com>
2024-01-18 13:26:33 +1100

    Add initialization delegate for mass agent component

--- Source/FPMass/Miscellaneous/FPMassAgentComponent.cpp
+++ Source/FPMass/Miscellaneous/FPMassAgentComponent.cpp
@@ -2,6 +2,42 @@
 
 #include "FPMassAgentComponent.h"
 
+#include "MassEntitySubsystem.h"
+
+UFPMassAgentComponent::UFPMassAgentComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = true;
+}
+
+bool UFPMassAgentComponent::IsAgentValid()
+{
+	const FMassEntityManager& EntityManager = GetWorld()->GetSubsystem<UMassEntitySubsystem>()->GetEntityManager();
+
+	if (!IsEntityPendingCreation())
+	{
+		if (AgentHandle.IsValid() && EntityManager.GetArchetypeForEntity(AgentHandle).IsValid())
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void UFPMassAgentComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// TODO should hook into PuppetInitializationDone or broadcast state changes but they aren't virtual 
+	if (bWaitingInitialized && IsAgentValid())
+	{
+		OnMassAgentInitialized.Broadcast();
+		SetComponentTickEnabled(false);
+		bWaitingInitialized = false;
+	}
+}
+
 void UFPMassAgentComponent::OnRegister()
 {
 	if (MassConfigAsset)
--- Source/FPMass/Miscellaneous/FPMassAgentComponent.h
+++ Source/FPMass/Miscellaneous/FPMassAgentComponent.h
@@ -6,17 +6,32 @@
 #include "MassAgentComponent.h"
 #include "FPMassAgentComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FFPOnMassAgentInitialized);
+
 UCLASS(ClassGroup=(FPMass), meta=(BlueprintSpawnableComponent))
 class FPMASS_API UFPMassAgentComponent : public UMassAgentComponent
 {
 	GENERATED_BODY()
 
 public:
+	UFPMassAgentComponent();
+
+	UPROPERTY(BlueprintAssignable)
+	FFPOnMassAgentInitialized OnMassAgentInitialized;
+
 	UPROPERTY(EditDefaultsOnly)
 	UMassEntityConfigAsset* MassConfigAsset = nullptr;
 
 	UFUNCTION(BlueprintCallable)
 	void SetMassConfig(UMassEntityConfigAsset* Config) { MassConfigAsset = Config; }
 
+	UFUNCTION(BlueprintCallable)
+	bool IsAgentValid();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	virtual void OnRegister() override;
+
+protected:
+	bool bWaitingInitialized = true;
 };
